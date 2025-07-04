f1397d9cdde0c58a0aa0780e93387f8ad0ac626f
tagreine <thomas.greiner@bouvet.no>
2025-07-04 08:40:59 +0200

    refactor: remove vector search part. split the endpoints. add health check endpoint

--- src/main/java/com/example/rag/controller/DocumentController.java
+++ src/main/java/com/example/rag/controller/DocumentController.java
@@ -1,11 +1,5 @@
 package com.example.rag.controller;
 
-import java.io.InputStream;
-import java.util.List;
-import java.util.UUID;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,24 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.rag.model.ChunkDocument;
-import com.example.rag.repository.ChunkRepository;
-import com.example.rag.service.EmbeddingService;
 import com.example.rag.service.StorageService;
-import com.example.rag.util.RAGTextUtils;
 
 @RestController
 @RequestMapping("/api")
 public class DocumentController {
     private final StorageService storageService;
-    private final EmbeddingService embeddingService;
-    private final ChunkRepository chunkRepository;
 
     @Autowired
-    public DocumentController(StorageService storageService, EmbeddingService embeddingService, ChunkRepository chunkRepository) {
+    public DocumentController(StorageService storageService) {
         this.storageService = storageService;
-        this.embeddingService = embeddingService;
-        this.chunkRepository = chunkRepository;
     }
 
     @GetMapping("/hello")
@@ -40,43 +26,27 @@ public class DocumentController {
         return "Hello from RAG backend!";
     }
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadPdf(@RequestParam("file") MultipartFile file) {
-        String originalFilename = file.getOriginalFilename();
-        if (file.isEmpty() || originalFilename == null || !originalFilename.endsWith(".pdf")) {
+    @GetMapping("/health/blob")
+    public ResponseEntity<String> blobContainerExist() {
+        if (this.storageService.blobServiceRunning()) {
+            return ResponseEntity.ok("Blob service is running.");
+        } else {
+            return ResponseEntity.status(503).body("Blob service is not available.");
+        }
+    }
+
+    @PostMapping("/ingest/pdf")
+    public ResponseEntity<String> uploadPdf(@RequestParam("file") MultipartFile pdfFile) {
+        String originalFilename = pdfFile.getOriginalFilename();
+        if (pdfFile.isEmpty() || originalFilename == null || !originalFilename.endsWith(".pdf")) {
             return ResponseEntity.badRequest().body("Please upload a PDF file.");
         }
         try {
-        String blobUrl = this.storageService.uploadPdf(file);
-        if (blobUrl == null || blobUrl.isEmpty()) {
-            return ResponseEntity.internalServerError().body("Failed to upload file.");
-        }
-
-        try (
-            InputStream pdfStream = this.storageService.downloadPdf(originalFilename);
-            PDDocument document = PDDocument.load(pdfStream);
-            ) {
-            
-            PDFTextStripper pdfStripper = new PDFTextStripper();
-            String extractedText = pdfStripper.getText(document);
-
-            List<String> chunks = RAGTextUtils.chunkText(extractedText, 500);
-            int count = 0;
-            for (String chunk : chunks) {
-                
-                List<Float> embedding = this.embeddingService.embed(chunk);
-                
-                ChunkDocument doc = new ChunkDocument();
-                doc.setId(UUID.randomUUID().toString());
-                doc.setDocumentName(originalFilename);
-                doc.setChunkText(chunk);
-                doc.setEmbedding(embedding);
-
-                this.chunkRepository.save(doc);
-                count++;
+            String blobUrl = this.storageService.uploadPdf(pdfFile);
+            if (blobUrl == null) {
+                return ResponseEntity.internalServerError().body("Failed to upload file to Azure Blob Storage.");
             }
-            return ResponseEntity.ok("File uploaded. Indexed " + count + " chunks.");
-        }
+            return ResponseEntity.ok("File uploaded successfully: " + blobUrl);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body("Failed to upload or process file.");
         }
