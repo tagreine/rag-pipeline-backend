ea6f66e6af7464e4789c07c80bbe3ce52f83bc83
tagreine <thomas.greiner@bouvet.no>
2025-07-04 08:42:19 +0200

    refactor: update StorageService interface

--- src/main/java/com/example/rag/service/StorageService.java
+++ src/main/java/com/example/rag/service/StorageService.java
@@ -4,7 +4,11 @@ import java.io.InputStream;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.azure.storage.blob.BlobClient;
+
 public interface StorageService {
+    boolean blobServiceRunning();
     String uploadPdf(MultipartFile file) throws Exception;
     InputStream downloadPdf(String originalFilename) throws Exception;
+    BlobClient getBlob(String containerName, String blob) throws Exception;
 }
